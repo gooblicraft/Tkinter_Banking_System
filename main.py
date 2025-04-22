63c86602c5aaa0bcbf88e00dd9433310e443a05b
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-22 17:09:57 +0800

    new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 mainWindow = Tk()
 mainWindow.title("Cash G Bank")
-mainWindow.geometry("300x400")
+mainWindow.geometry("400x500")
 
 title_page = Label(mainWindow, text="Welcome to Cash G Bank", font=("Arial", 12))
 title_page.pack(side="top", padx=5)
