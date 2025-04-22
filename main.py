3ed75875edac8acc6f5627f0cdc733ef911f8b00
Jabiii1 <jayveegarcia018@gmail.com>
2025-04-22 15:31:09 +0800

    Update main.py
    new

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
 
 mainWindow = Tk()
-mainWindow.title("BRat Banking Sytem")
+mainWindow.title("BRat Bank")
 mainWindow.geometry("300x400")
 
-
+title_page = Label(mainWindow, text="Welcome to BRat Bank", font=("Arial", 12))
+title_page.pack(side="top", padx=5)
 
 mainWindow.mainloop()
